d9e3f1437aec21d4fa1facf61b4285783833a3a0
Sunghyun Cho <mail@chosunghyun.com>
2023-03-19 18:20:44 -0700

    Add image proxy and Twitter API endpoints

---
+++ src/pages/api/imageProxy.ts
@@ -0,0 +1,6 @@
+import { withImageProxy } from '@blazity/next-image-proxy'
+
+export default withImageProxy({
+  // pbs.twimg.com is a default whitelisted pattern
+  whitelistedPatterns: ['pbs.twimg.com'],
+})
---
+++ src/pages/api/twitter.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const { q } = req.query
+
+    const response = await fetch(`https://api.twitter.com/1.1/search/tweets.json?q=${q}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      },
+    })
+
+    const data = await response.json()
+    res.status(200).json(data)
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).json({ error: 'Something went wrong' })
+  }
+}
